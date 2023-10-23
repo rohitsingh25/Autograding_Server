90f77d9e55f948713fdbe265204fc18e5aec8b88
rohit <rsy708943@gmail.com>
2023-10-24 00:51:39 +0530

    lab 6 added

--- Lab 6/client.cpp
+++ Lab 6/client.cpp
@@ -5,16 +5,14 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <fstream>
-#include <sys/time.h>
 
-using namespace std; 
+using namespace std; // Add this line to avoid using std:: everywhere
 
-const int MAX_BUFFER_SIZE = 10000;
-int success=0;
- 
+const int MAX_BUFFER_SIZE = 1024;
+
 int main(int argc, char* argv[]) {
-    if (argc != 5) {
-        cerr << "Usage: " << argv[0] << " <serverIP:port> <sourceCodeFileTobeGraded> <loopNum> <sleepTimeSeconds>" << endl;
+    if (argc != 3) {
+        cerr << "Usage: " << argv[0] << " <serverIP:port> <sourceCodeFileTobeGraded>" << endl;
         return 1;
     }
 
@@ -26,9 +24,6 @@ int main(int argc, char* argv[]) {
     struct sockaddr_in serverAddr;
 
     // Create socket
-     // Connect to the server
-    
-    
     if ((clientSocket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         cerr << "Error creating socket." << endl;
         return 1;
@@ -38,67 +33,40 @@ int main(int argc, char* argv[]) {
     serverAddr.sin_port = htons(atoi(serverPort));
     serverAddr.sin_addr.s_addr = inet_addr(serverAddress);
 
-    
+    // Connect to the server
     if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) == -1) {
         cerr << "Error connecting to server." << endl;
         return 1;
     }
+
     // Read the source code file
     ifstream file(sourceCodeFile);
     string sourceCode((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
     file.close();
-    
-    
-    int lp=atoi(argv[3]);
-    int sl=atoi(argv[4]);
-    char buffer[MAX_BUFFER_SIZE];
-    timeval Tsend,Trecv,diff,sum,ti,te;
-    sum.tv_sec=0,sum.tv_usec=0;
-    float suc=0;
-    gettimeofday(&ti, NULL);
-    
-    
-    for(int i=0;i<lp;i++){
+
     // Send the source code to the server
-    gettimeofday(&Tsend, NULL);
     ssize_t bytesSent = send(clientSocket, sourceCode.c_str(), sourceCode.size(), 0);
     if (bytesSent == -1) {
         cerr << "Error sending data." << endl;
         close(clientSocket);
         return 1;
     }
-    
+
+    char buffer[MAX_BUFFER_SIZE];
     memset(buffer, 0, sizeof(buffer));
+
     // Receive result from the server
-    ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer)-1, 0);
-  
+    ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
     if (bytesRead == -1) {
         cerr << "Error receiving data." << endl;
         close(clientSocket);
         return 1;
     }
-    else suc++;
-    gettimeofday(&Trecv, NULL);
+
     cout << "Autograding Result: " << buffer << endl;
-    diff.tv_sec = Trecv.tv_sec - Tsend.tv_sec;
-    diff.tv_usec=Trecv.tv_usec-Tsend.tv_usec;
-    if (diff.tv_usec < 0) {
-        diff.tv_sec--;
-        diff.tv_usec += 1000000; // 1,000,000 microseconds in a second
-    }
-    
-    sum.tv_usec+=diff.tv_usec;
-    if (sum.tv_usec >= 1000000) {
-        sum.tv_sec++;
-        sum.tv_usec -= 1000000;
-    }
-   
-    sleep(sl);
- }
-    gettimeofday(&te, NULL);
-    
+
     close(clientSocket);
-    cout<<"Number of successful responses:"<<suc<<"\ntotal time elapsed between each request and response in microseconds: "<<sum.tv_usec<<"\nAvg response time in microseconds: "<<sum.tv_usec/float(lp)<<"\nThroughput:"<<(suc)*(1000000.0/sum.tv_usec)<<"\ntime taken by loop in seconds: "<<te.tv_sec-ti.tv_sec<<"\n";
+
     return 0;
 }
 
--- Lab 6/server.cpp
+++ Lab 6/server.cpp
@@ -8,16 +8,18 @@
 #include <cstdio>
 #include <fcntl.h>
 
+using namespace std; // Add this line to avoid using std:: everywhere
 
-using namespace std;
-
-const int MAX_BUFFER_SIZE = 10000;
+const int MAX_BUFFER_SIZE = 1024;
 
 bool isOutputCorrect(const string& programOutput) {
-    string expectedOutput = "1 2 3 4 5 6 7 8 9 10";
+    string expectedOutput = "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n";
     return programOutput == expectedOutput;
 }
-
+int fd=open("output.txt",O_WRONLY | O_TRUNC | O_CREAT, 0644);
+string x="1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n";
+const char *y = x.c_str();
+int z=write(fd,y,x.size());
 int main(int argc, char* argv[]) {
     if (argc != 2) {
         cerr << "Usage: " << argv[0] << " <port>" << endl;
@@ -45,44 +47,29 @@ int main(int argc, char* argv[]) {
     }
 
     // Listen for incoming connections
-    if (listen(serverSocket, 100) == -1) {
+    if (listen(serverSocket, 5) == -1) {
         cerr << "Error listening for connections." << endl;
         return 1;
     }
 
     cout << "Server listening on port " << atoi(argv[1]) << "..." << endl;
-    
-     int fd=open("output.txt",O_WRONLY | O_TRUNC | O_CREAT, 0644);
-     string x="1 2 3 4 5 6 7 8 9 10";
-     const char *y = x.c_str();
-     int z=write(fd,y,x.size());
-     if (close(fd) == -1) {
-        perror("close");
-        return 1;
-    }
-     while(true) {
+
+    while (true) {
         // Accept incoming connection
-        
         clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrLen);
         if (clientSocket == -1) {
             cerr << "Error accepting connection." << endl;
             continue;
-            }
-       
-        while(true){
+        }
+
         char buffer[MAX_BUFFER_SIZE];
         memset(buffer, 0, sizeof(buffer));
+
         // Receive source code from client
         ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
-        if (bytesRead == 0 || bytesRead == -1) {
-            //cerr << "Error receiving data." << endl;
-            //closing connection with client when no data is sent by it
+        if (bytesRead == -1) {
+            cerr << "Error receiving data." << endl;
             close(clientSocket);
-            clientSocket = accept(serverSocket, (struct sockaddr*)&clientAddr, &clientAddrLen);
-            if (clientSocket == -1) {
-            cerr << "Error accepting connection." << endl;
-            continue;
-            }
             continue;
         }
 
@@ -90,32 +77,21 @@ int main(int argc, char* argv[]) {
         ofstream codeFile("student_code.cpp");
         codeFile << buffer;
         codeFile.close();
-        ifstream eFile("student_code.cpp");
-        string er((istreambuf_iterator<char>(eFile)), istreambuf_iterator<char>());
+
         // Compile and run the code
         int compileResult = system("g++ -o student_program student_code.cpp 2> compile_error.txt");
-        if (remove("student_code.cpp") != 0) {
-            cerr << "Error deleting temporary code file." << endl;
-            }
+
         if (compileResult != 0) {
             // Compilation failed, send the error message back
             ifstream errorFile("compile_error.txt");
             string compileError((istreambuf_iterator<char>(errorFile)), istreambuf_iterator<char>());
             string response = "COMPILER ERROR\n" + compileError;
             send(clientSocket, response.c_str(), response.size(), 0);
-            errorFile.close();
-             if (remove("compile_error.txt") != 0) {
-            cerr << "Error deleting temporary code file." << endl;
-            }
-            //close(clientSocket);
-            
+            close(clientSocket);
             continue;
         }
-        if (remove("compile_error.txt") != 0) {
-            cerr << "Error deleting temporary code file." << endl;
-            }
 
-        int executionResult = system("./student_program > student_output.txt 2>runtime_error.txt");
+        int executionResult = system("./student_program 1>student_output.txt 2>runtime_error.txt");
 
         if (executionResult != 0) {
             // Runtime error, send the error message back
@@ -123,20 +99,14 @@ int main(int argc, char* argv[]) {
             string runtimeError((istreambuf_iterator<char>(err)), istreambuf_iterator<char>());
             string response = "RUNTIME ERROR\n"+runtimeError;
             send(clientSocket, response.c_str(), response.size(), 0);
-            err.close();
-            //close(clientSocket);
-            if (remove("runtime_error.txt") != 0) {
-            cerr << "Error deleting temporary code file." << endl;
-            }
+            close(clientSocket);
             continue;
         }
-        if (remove("runtime_error.txt") != 0) {
-            cerr << "Error deleting temporary code file." << endl;
-            }
+
         // Capture the program's output
         ifstream output_file("student_output.txt");
         string programOutput((istreambuf_iterator<char>(output_file)), istreambuf_iterator<char>());
-        output_file.close();
+
         // Check if the output is correct
         bool outputCorrect = isOutputCorrect(programOutput);
 
@@ -148,24 +118,13 @@ int main(int argc, char* argv[]) {
         string OutErr((istreambuf_iterator<char>(err)), istreambuf_iterator<char>());
         OutErr="OUTPUT ERROR\n"+OutErr;
         send(clientSocket, OutErr.c_str(), OutErr.size(), 0);
-        err.close();
-        if (remove("output_error.txt") != 0) {
-            cerr << "Error deleting temporary code file." << endl;
-            }
-         
-        }
-        else{send(clientSocket, response.c_str(), response.size(), 0);   }
-         if (remove("student_output.txt") != 0) {
-            cerr << "Error deleting temporary code file." << endl;
-            }
-        }     
-        }
+        close(clientSocket);
+    }
+        else{send(clientSocket, response.c_str(), response.size(), 0);close(clientSocket);}
         
-      
-    
-     
-     //close(clientSocket);
-     //close(serverSocket);
+   
+    }
+     close(serverSocket);
     return 0;
 }
 
